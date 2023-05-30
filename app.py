5d027ff7efd3a2bf0b57570aeb58491268f76c1c
Gollavishnuyadav <gollavishnuyadav1@gmail.com>
2023-05-30 17:45:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,request
+
+
+from flask_wtf import Form
+
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+FAI=Flask(__name__)
+class NameForm(Form):
+    Sname=StringField(validators=[DataRequired()])
+    Submit=SubmitField()
+
+@FAI.route('/webforms',methods=['GET','POST'])
+def webforms():
+    NFD=NameForm()
+    if request.method=='POST':
+        NFD=NameForm(request.form)
+        if NFD.validate():
+            return NFD.Sname.data
+    return render_template('webform.html',NFD=NFD)
+
+if __name__=='__main__':
+    FAI.run(debug=True)
+
+# from flask import Flask,render_template,request
+# from flask_wtf import Form
+
+# from wtforms import StringField,SubmitField
+# from wtforms.validators import DataRequired
+# FAI=Flask(__name__)
+# class NameForm(Form):
+#     name=StringField(validators=[DataRequired()])
+#     submit=SubmitField()
+
+# @FAI.route('/webforms',methods=['GET','POST'])
+# def webforms():
+#     NFO=NameForm()
+#     if request.method=='POST':
+#         NFD=NameForm(request.form)
+#         if NFD.validate():
+#             return NFD.name.data
+
+#     return render_template('webforms.html',NFO=NFO)
+# if __name__=='__main__':
+#     FAI.run(debug=True)
